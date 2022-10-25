dadcb112f7306cf0664a8093e62e7859f04da819
Dave Sexton <davesextonjr@gmail.com>
2022-10-25 12:06:21 -0400

    finish seed data for demo bookings

--- backend/db/seeders/20221025020323-demo-bookings.js
+++ backend/db/seeders/20221025020323-demo-bookings.js
@@ -3,13 +3,38 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert
+    return queryInterface.bulkInsert('Bookings', [
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: "2022-11-05",
+        endDate: "2022-11-07"
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: "2022-11-08",
+        endDate: "2022-11-09"
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: "2022-11-05",
+        endDate: "2022-11-07"
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate: "2022-11-08",
+        endDate: "2022-11-09"
+      },
+    ])
   },
 
   async down (queryInterface, Sequelize) {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Spots', {
-      name: { [Op.in]: ["Fire House", "German House", "Ice House"] }
+    return queryInterface.bulkDelete('Bookings', {
+      spotId: { [Op.in]: [1,2] }
     }, {});
   }
 };
