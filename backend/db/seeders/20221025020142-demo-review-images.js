bd785b68bfc1cc8e3ce69165e53aefab6ac69c02
Dave Sexton <davesextonjr@gmail.com>
2022-10-25 11:56:15 -0400

    create review image seed data

--- backend/db/seeders/20221025020142-demo-review-images.js
+++ backend/db/seeders/20221025020142-demo-review-images.js
@@ -3,13 +3,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert
+    return queryInterface.bulkInsert('ReviewImages', [
+      {
+        reviewId: 1,
+        url: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg"
+      },
+      {
+        reviewId: 2,
+        url: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg"
+      },
+      {
+        reviewId: 1,
+        url: "https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?"
+      },
+      {
+        reviewId: 1,
+        url: "https://images.pexels.com/photos/2980955/pexels-photo-2980955.jpeg"
+      },
+      {
+        reviewId: 2,
+        url: "https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg"
+      },
+    ])
   },
 
   async down (queryInterface, Sequelize) {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Spots', {
-      name: { [Op.in]: ["Fire House", "German House", "Ice House"] }
+    return queryInterface.bulkDelete('ReviewImages', {
+      reviewId: { [Op.in]: [1, 2] }
     }, {});
   }
 };
