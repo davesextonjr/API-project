e30ad89f4cab5739db578079abdc043248f11098
Dave Sexton <davesextonjr@gmail.com>
2022-10-25 19:32:06 -0400

    comment in the environment variable

--- backend/app.js
+++ backend/app.js
@@ -70,7 +70,7 @@ app.use((_req, _res, next) => {
       title: err.title || 'Server Error',
       message: err.message,
       errors: err.errors,
-      //stack: isProduction ? null : err.stack
+      stack: isProduction ? null : err.stack
 
     });
   });
