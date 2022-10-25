8616da5522b644db0edfddb58347f050497789ea
Dave Sexton <davesextonjr@gmail.com>
2022-10-25 19:08:49 -0400

    test production environment

--- backend/app.js
+++ backend/app.js
@@ -65,11 +65,13 @@ app.use((_req, _res, next) => {
   app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     console.error(err);
+    console.log("!!!!!!!!!!!!!!!",isProduction)
     res.json({
       title: err.title || 'Server Error',
       message: err.message,
       errors: err.errors,
       stack: isProduction ? null : err.stack
+
     });
   });
 
