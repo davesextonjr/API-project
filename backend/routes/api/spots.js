b94347a5041bb18a7dabfec40371476986157588
Dave Sexton <davesextonjr@gmail.com>
2022-10-27 12:59:56 -0400

    something weird is happening

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -47,49 +47,47 @@ const validateSpotSignup =[
 router.get('/', async (req, res, next) => {
     const responseBody = [];
     const spots = await Spot.findAll({
-        include:[
-            {
-                model: SpotImage,
-                attributes: ['url'],
-                where: {
-                    preview: true
-                }
-            },
-            {
-                model: Review,
-                attributes:['stars'] //collect for the avg
-            }
-        ]
-        })
+        // include:[
+        //     {
+        //         model: SpotImage,
+        //         attributes: ['url'],
+        //         where: {
+        //             preview: true
+        //         }
+        //     },
+        //     {
+        //         model: Review,
+        //         attributes:['stars'] //collect for the avg
+        //     }
+        // ]
+        // })
 
-        spots.forEach(obj => {
-            let sum = 0; //get the avg review
-            obj.Reviews.forEach(review => {
-                sum += review.stars
-            })
-            const avgRating = sum / obj.Reviews.length
+        // spots.forEach(obj => {
+        //     let sum = 0; //get the avg review
+        //     obj.Reviews.forEach(review => {
+        //         sum += review.stars
+        //     })
+        //     const avgRating = sum / obj.Reviews.length
 
-            responseBody.push({
-                id: obj.id,
-                ownerId: obj.ownerId,
-                address: obj.address,
-                city: obj.city,
-                state: obj.state,
-                country: obj.country,
-                lat: obj.lat,
-                lng: obj.lng,
-                name: obj.name,
-                description: obj.description,
-                price: obj.price,
-                createdAt: obj.createdAt,
-                updatedAt: obj.updatedAt,
-                avgRating: avgRating,
-                previewImage: obj.SpotImages[0].url
-            })
+        //     responseBody.push({
+        //         id: obj.id,
+        //         ownerId: obj.ownerId,
+        //         address: obj.address,
+        //         city: obj.city,
+        //         state: obj.state,
+        //         country: obj.country,
+        //         lat: obj.lat,
+        //         lng: obj.lng,
+        //         name: obj.name,
+        //         description: obj.description,
+        //         price: obj.price,
+        //         createdAt: obj.createdAt,
+        //         updatedAt: obj.updatedAt,
+        //         avgRating: avgRating,
+        //         previewImage: obj.SpotImages[0].url
+        //     })
         })
-    res.json({
-        Spots: responseBody
-    })
+    res.json(spots)
 });
 
 router.post('/', authenticateUser, validateSpotSignup, async (req, res, next) => {
@@ -159,13 +157,15 @@ router.post('/:spotId/reviews', authenticateUser, async (req, res, next) => {
     res.json(newReview)
 })
 
-router.get('/spots/current', requireAuth, async (req, res) => {
+router.get('/current', authenticateUser, async (req, res) => {
     const userId = req.user.id;
     const spots = await Spot.findAll({
         where: {
             ownerId: userId
         }
     })
+
+router.get('/:spotId')
 
     res.json(spots)
 })
