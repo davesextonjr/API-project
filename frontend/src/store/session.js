85327828a2de1e59242c78541df8be9c2bac0a2f
Dave Sexton <davesextonjr@gmail.com>
2022-11-11 18:35:33 -0500

    Complete login logic

---
+++ frontend/src/store/session.js
@@ -0,0 +1,51 @@
+import { csrfFetch } from "./csrf";
+
+const LOGIN_USER = 'session/LOGIN_USER';
+const LOGOUT_USER = 'session/LOGOUT_USER';
+
+//actions
+    export const loginUser = (user) => {
+        return{
+            type: LOGIN_USER,
+            user
+        }
+    }
+
+    export const logoutUser = () => {
+        return {type: LOGOUT_USER}
+    }
+
+
+
+
+
+//thunks
+    export const loginUserThunk = (credential, password) => async (dispatch) => {
+        const response = await csrfFetch ('api/session', {
+            method: 'POST',
+            body: JSON.stringify({credential, password})
+        })
+        if (!response.ok) return response.json()
+        else {
+        const user = await response.json();
+            dispatch(loginUser(user));
+            return user;
+        }
+    }
+
+
+//reducer
+const initialState = {user: null}
+
+export default function sessionReducer(state = initialState, action) {
+    switch(action.type) {
+        case LOGIN_USER: {
+            return {...state,  user: action.user}
+        }
+        case LOGOUT_USER: {
+            return initialState
+        }
+        default: return state
+    }
+
+}
