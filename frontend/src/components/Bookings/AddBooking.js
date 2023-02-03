510c01bb5658a93e4f77b549c2782b3d7c5ece6e
David Sexton Jr <davesextonjr@gmail.com>
2023-02-03 15:50:46 -0500

    deal with error handeling on reservations

--- frontend/src/components/Bookings/AddBooking.js
+++ frontend/src/components/Bookings/AddBooking.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-// import { ErrorMapper } from "../utilities";
+import { csrfFetch } from "../../store/csrf";
 
 export default function AddBooking() {
-    const {spotId} = useParams
-    const dispatch = useDispatch
+    const {spotId} = useParams()
     const currentUserObject = useSelector(state => state.session)
 
     const [errors, setErrors] = useState([])
@@ -14,16 +13,31 @@ export default function AddBooking() {
 
     const onSubmit = async e => {
         e.preventDefault();
-        const booking = {
-            startDate,
-            endDate
-        }
-        console.log(booking)
+        setErrors([]);
+        console.log(errors)
+        const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        const currentDay = daysOfTheWeek[new Date(startDate).getDay()]
+        console.log(currentDay)
+
+        const lengthOfStay = (new Date(endDate).getTime() - new Date(startDate).getTime())/86400000
+        await csrfFetch(`/api/spots/${spotId}/bookings`, {
+            method: 'POST',
+            body: JSON.stringify({
+                startDate,
+                endDate
+            })
+        }).catch( async res => {
+            const data = await res.json()
+            console.log(data)
+            if (data && data.errors) setErrors(data.errors);
+        })
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            {/* <ErrorMapper errArray={errors}/> */}
+        <form id="bookings" onSubmit={onSubmit}>
+              <ul>
+                {errors.map((err) => <li key={err}>{err}</li>)}
+            </ul>
             <label htmlFor="start-date">Check-in</label>
             <input
                 type="date"
