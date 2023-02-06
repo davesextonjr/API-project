11725b3d78eb80a403163535a9f48673906dd818
David Sexton Jr <davesextonjr@gmail.com>
2023-02-06 13:45:28 -0500

    add delete booking feature

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -7,7 +7,7 @@ import { dateFormatter } from "../utilities";
 
 export function UserBookings() {
     const [isLoaded, setIsLoaded] = useState(false)
-    const [bookings, setBookings] = useState ([])
+    const [bookings, setBookings] = useState([])
     const history = useHistory()
     useEffect(() => {
         (async () => {
@@ -16,18 +16,30 @@ export function UserBookings() {
             setBookings(fetchedBookings.Bookings)
             setIsLoaded(true)
         })()
+    }, [isLoaded])
 
 
-        // .then((res) => setBookings(res.json())).then(() => setIsLoaded(true))
-    },[isLoaded])
-    console.log(bookings)
+    const deleteHandler = async e => {
+        e.preventDefault()
+        const res = await csrfFetch(`/api/bookings/${e.target.id}`, {
+            method: 'DELETE'
+        })
+        if(res.ok) setIsLoaded(false)
+    }
+
+
     return isLoaded && (
         <div className="bookingsContainer">
             <div className="title">Your Upcoming Stays</div>
             <div className="sub-title">Click on a reservation to see that spot's details</div>
             {bookings.length && bookings.map(booking => {
                 return (
-                    <div key={`booking-${booking.id}`} onClick={(() => history.push(`/spots/${booking.spotId}`))}>{dateFormatter(booking.startDate, booking.endDate)}</div>
+                    <div key={`booking-${booking.id}`}>
+                        <div onClick={(() => history.push(`/spots/${booking.spotId}`))}>{dateFormatter(booking.startDate, booking.endDate)}</div>
+                        <button id={booking.id}
+                            onClick={deleteHandler}
+                        >Delete</button>
+                    </div>
                 )
             })}
         </div>
