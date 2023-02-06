ca80891649fdc7aecaebc6df66feb72a27f0e792
David Sexton Jr <davesextonjr@gmail.com>
2023-02-06 12:17:56 -0500

    create fetch users bookings

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -7,8 +7,17 @@ export function UserBookings() {
     const [isLoaded, setIsLoaded] = useState(false)
     const [bookings, setBookings] = useState ([])
     useEffect(() => {
-        csrfFetch('').then(() => setIsLoaded(true))
-    })
+        (async () => {
+            const res = await csrfFetch('/api/bookings/current')
+            const fetchedBookings = await res.json()
+            setBookings(fetchedBookings)
+            setIsLoaded(true)
+        })()
+
+
+        // .then((res) => setBookings(res.json())).then(() => setIsLoaded(true))
+    },[isLoaded])
+    console.log(bookings)
     return isLoaded && (
         <>
             <div className="tester">Here</div>
