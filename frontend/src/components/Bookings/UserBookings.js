f81d676d047430a8ff3fb9870b48a599deda3875
David Sexton Jr <davesextonjr@gmail.com>
2023-02-06 13:20:22 -0500

    create a day formatter helper function

--- frontend/src/components/Bookings/UserBookings.js
+++ frontend/src/components/Bookings/UserBookings.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { csrfFetch } from "../../store/csrf";
+import { dateFormatter } from "../utilities";
 
 
 
 export function UserBookings() {
     const [isLoaded, setIsLoaded] = useState(false)
     const [bookings, setBookings] = useState ([])
+    const history = useHistory()
     useEffect(() => {
         (async () => {
             const res = await csrfFetch('/api/bookings/current')
             const fetchedBookings = await res.json()
-            setBookings(fetchedBookings)
+            setBookings(fetchedBookings.Bookings)
             setIsLoaded(true)
         })()
 
@@ -19,8 +22,15 @@ export function UserBookings() {
     },[isLoaded])
     console.log(bookings)
     return isLoaded && (
-        <>
-            <div className="tester">Here</div>
-        </>
+        <div className="bookingsContainer">
+            <div className="title">Your Upcoming Stays</div>
+            <div className="sub-title">Click on a reservation to see that spot's details</div>
+            {bookings.length && bookings.map(booking => {
+                return (
+                    <div key={`booking-${booking.id}`} onClick={(() => history.push(`/spots/${booking.spotId}`))}>{dateFormatter(booking.startDate, booking.endDate)}</div>
+                )
+            })}
+        </div>
     )
+
 }
--- frontend/src/components/utilities/index.js
+++ frontend/src/components/utilities/index.js
@@ -5,3 +5,17 @@ export function ErrorMapper({errArray}){
         </ul>
     )
 }
+
+export function dateFormatter(start, end){
+    const daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    const months = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+    const startArray = start.split("-")
+    const lengthOfStay = (new Date(end).getTime() - new Date(start).getTime())/86400000
+    const currentDay = daysOfTheWeek[new Date(start).getDay()]
+
+
+
+    return(`${currentDay}, ${months[+(startArray[1]) - 1]} ${startArray[2]} for ${lengthOfStay} nights`)
+}
